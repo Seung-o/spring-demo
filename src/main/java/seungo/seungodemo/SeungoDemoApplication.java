f39449c2af6dc888b1bcdd3d7e5db9900b1bca71
seungo <ilkhso@gmail.com>
2024-01-28 20:10:24 +0900

    init

---
+++ src/main/java/seungo/seungodemo/SeungoDemoApplication.java
@@ -0,0 +1,13 @@
+package seungo.seungodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeungoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeungoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seungo/seungodemo/SeungoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package seungo.seungodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
