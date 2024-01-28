9326287b9a1366761d524fd5395275bcbeda4e32
seungo <ilkhso@gmail.com>
2024-01-29 00:19:30 +0900

    feat: member service

---
+++ src/main/java/seungo/seungodemo/service/MemberService.java
@@ -0,0 +1,44 @@
+package seungo.seungodemo.service;
+
+import seungo.seungodemo.domain.Member;
+import seungo.seungodemo.repository.MemberRepository;
+import seungo.seungodemo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     *
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return
+     */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
